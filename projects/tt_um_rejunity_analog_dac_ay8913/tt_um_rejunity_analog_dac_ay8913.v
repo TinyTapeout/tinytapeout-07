61ef75373fc604b30dba236bc2b12fac78cb9f33
TinyTapeoutBot <139130078+TinyTapeoutBot@users.noreply.github.com>
2024-05-23 19:37:11 +0300

    feat: update project tt_um_rejunity_analog_dac_ay8913 from rejunity/tt07-analog-dac-ay8913
    Commit: e7772e17ba5ceb77bf45642a70abe2d5debbab90
    Workflow: https://github.com/rejunity/tt07-analog-dac-ay8913/actions/runs/9210996292

---
+++ projects/tt_um_rejunity_analog_dac_ay8913/tt_um_rejunity_analog_dac_ay8913.v
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2024 ReJ aka Renaldas Zioma
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module tt_um_rejunity_analog_dac_ay8913 (
+    input  wire       VGND,
+    input  wire       VPWR,
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    inout  wire [7:0] ua,       // Analog pins, only ua[5:0] can be used
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+endmodule
